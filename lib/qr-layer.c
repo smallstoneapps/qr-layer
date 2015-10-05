92cba423abc169b250e27146ca89de84e1da7ad4
Ferenc Szekely <ferenc.szekely@gmail.com>
2015-10-05 14:15:46 +0300

    clear the layer before redrawing it

--- lib/qr-layer.c
+++ lib/qr-layer.c
@@ -1,12 +1,7 @@
 #include <pebble.h>
 #include "qr-layer.h"
-#include <QR_Encode.h>
 
-typedef struct QRData {
-  unsigned char str[MAX_BITDATA];
-  uint8_t width;
-} QRData;
-
+static void clear(Layer* layer, GContext* ctx);
 static void update(Layer* layer, GContext* ctx);
 
 QRLayer* qr_layer_create(GRect bounds) {
@@ -33,9 +28,20 @@ Layer* qr_layer_get_layer(QRLayer* layer) {
   return (Layer*)layer;
 }
 
+static void clear(Layer* layer, GContext* ctx) {
+  GRect layer_size = layer_get_bounds(layer);
+  graphics_context_set_fill_color(ctx, DEFAULT_BG_COLOR);
+  graphics_fill_rect(ctx, layer_size, 0, GCornerNone);
+  graphics_context_set_fill_color(ctx, DEFAULT_FILL_COLOR);
+}
+
 static void update(Layer* layer, GContext* ctx) {
+  GRect layer_size = layer_get_bounds(layer);
+
+  clear(layer, ctx);
+
   QRData* qr_data = (QRData*) layer_get_data(layer);
-  
+
   if (qr_data->width == 0) {
     return;
   }
@@ -43,7 +49,6 @@ static void update(Layer* layer, GContext* ctx) {
 	uint8_t byte = 0;
 	uint8_t bit = 7;
 
-	GRect layer_size = layer_get_bounds(layer);
 	uint8_t block_size = layer_size.size.w / qr_data->width;
 	uint8_t padding_x = (layer_size.size.w - (block_size * qr_data->width)) / 2;
 	uint8_t padding_y = (layer_size.size.h - (block_size * qr_data->width)) / 2;
